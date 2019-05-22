22fcdb1eb6513c4edb123894568fe9846eff6127
TigranM <margaryantigran4@gmail.com>
2019-05-22 13:23:44 +0400

    Bug Fixed

--- onlineBank/onlinebank-web/src/main/java/onlineBank/web/OnlineBankApplication.java
+++ onlineBank/onlinebank-web/src/main/java/onlineBank/web/OnlineBankApplication.java
@@ -1,17 +1,19 @@
 package onlineBank.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+@Configuration
+@EnableAutoConfiguration
 @EntityScan("onlineBank.*")
 @EnableConfigurationProperties
 @EnableJpaRepositories("onlineBank.*")
 @EnableTransactionManagement(proxyTargetClass = true)
-@SpringBootApplication(scanBasePackages = {"onlineBank.*"})
 public class OnlineBankApplication {
 
 	public static void main(String[] args) {
